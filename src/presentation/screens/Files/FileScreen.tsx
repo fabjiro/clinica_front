6f99ae91c84ed1059afe6a4b69ba08e4c2713366
fabjiro <fjiron2001@gmail.com>
2024-12-13 15:03:19 -0600

    patient list

--- src/presentation/screens/Files/FileScreen.tsx
+++ src/presentation/screens/Files/FileScreen.tsx
@@ -1,8 +1,75 @@
 import { Button } from "@nextui-org/react";
 import { BaseScreen } from "../BaseScreen";
 import { MdAdd } from "react-icons/md";
+import { useGetAllPatient } from "./query/patient.query";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useMemo } from "react";
+import { ActionPatient } from "./components/ActionPatient";
+
+const columns: GridColDef[] = [
+  {
+    field: "col1",
+    headerName: "Nombre",
+    flex: 1,
+  },
+  {
+    field: "col2",
+    headerName: "Telefono",
+    flex: 1,
+  },
+  {
+    field: "col3",
+    headerName: "Dirreccion",
+    flex: 1,
+  },
+  {
+    field: "col4",
+    headerName: "Telefono",
+    flex: 1,
+  },
+  {
+    field: "col5",
+    headerName: "Sexo",
+    flex: 1,
+  },
+  {
+    field: "col6",
+    headerName: "Estado Civil",
+    flex: 1,
+  },
+  {
+    field: "col7",
+    headerName: "Acciones",
+    width: 100,
+    sortable: false,
+    filterable: false,
+    pinnable: false,
+    renderCell: (params) => <ActionPatient id={params.id.toString()} />,
+  },
+];
 
 export function FileScreen() {
+  const { 
+    data: dataPatient,
+    status: statusGetPatient,
+   } = useGetAllPatient();
+
+   const row = useMemo(() => {
+      if(!dataPatient) return [];
+
+      return dataPatient.map((patient) => ({
+        id: patient.id,
+        col1: patient.name,
+        col2: patient.phone,
+        col3: patient.address,
+        col4: patient.phone,
+        col5: patient.typeSex === "c2594acf-bb7c-49d0-9506-f556179670ab" ? "Masculino" : "Femenino",
+        col6: patient.civilStatus.name,
+      }));
+   }, [dataPatient]);
+
+   const isLoadingPatient = statusGetPatient === "pending";
+
   return (
     <BaseScreen
       titlePage="Expedientes"
@@ -12,7 +79,9 @@ export function FileScreen() {
         </>
       }
     >
-      <p>cuerpo</p>
+      <div className="flex-1 overflow-auto">
+        <DataGrid loading={isLoadingPatient} columns={columns} rows={row} disableColumnMenu hideFooter />
+      </div>
     </BaseScreen>
   );
 }
---
+++ src/presentation/screens/Files/components/ActionPatient.tsx
@@ -0,0 +1,42 @@
+import { Dropdown, DropdownTrigger, Button, DropdownMenu, DropdownItem } from "@nextui-org/react";
+import { FaEllipsisVertical } from "react-icons/fa6";
+import { MdEdit, MdDelete } from "react-icons/md";
+
+interface IProps {
+  id: string;
+}
+
+export function ActionPatient({ id }: IProps) {
+  return (
+    <div className="flex flex-row gap-2 items-center justify-center h-full w-full">
+      <Dropdown backdrop="blur" className="rounded-md">
+        <DropdownTrigger>
+          <Button
+            size="sm"
+            isIconOnly
+            variant="light"
+          >
+            <FaEllipsisVertical />
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu>
+          <DropdownItem
+            showDivider
+            startContent={<MdEdit />}
+            key="edit"
+          >
+            Editar
+          </DropdownItem>
+          <DropdownItem
+            className="text-danger"
+            color="danger"
+            key="edit"
+            startContent={<MdDelete />}
+          >
+            Eliminar
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  );
+}
