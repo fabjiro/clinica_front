51ae65b996c7c5e4c3d26d5e67ce70ccfd94df1b
git config --global user.name Jorge <eduardotreminio10.com>
2025-01-15 19:56:37 -0600

    graficos

--- src/presentation/screens/Dashboard/DashboardScreen.tsx
+++ src/presentation/screens/Dashboard/DashboardScreen.tsx
@@ -1,13 +1,87 @@
+import { LineChart } from "@mui/x-charts";
 import { BaseScreen } from "../BaseScreen";
+import { BarChart } from "@mui/x-charts/BarChart";
 
 export function DashboardScreen() {
-    return (<div>
-        <BaseScreen
-        titlePage="Dashboard"
-        >
-        <p>cuerpo del dashboard</p>
-        </BaseScreen>
+  const chartSetting = {
+    yAxis: [
+      {
+        label: "rainfall (mm)",
+      },
+    ],
+    series: [{ dataKey: "seoul", label: "Seoul rainfall" }],
+    height: 300,
+    // sx: {
+    //   [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
+    //     transform: 'translateX(-10px)',
+    //   },
+    // },
+  };
 
+  const dataset = [
+    {
+      london: 10,
+      paris: 10,
+      newYork: 10,
+      seoul: 10,
+      month: "Jan",
+    },
+    {
+      london: 10,
+      paris: 10,
+      newYork: 10,
+      seoul: 10,
+      month: "Feb",
+    },
+    {
+      london: 10,
+      paris: 10,
+      newYork: 10,
+      seoul: 10,
+      month: "Mar",
+    },
+    {
+      london: 10,
+      paris: 10,
+      newYork: 10,
+      seoul: 10,
+      month: "Apr",
+    },
+    {
+      london: 10,
+      paris: 10,
+      newYork: 10,
+      seoul: 10,
+      month: "May",
+    },
+  ];
 
-    </div>);
+  return (
+    <div>
+      <BaseScreen titlePage="Dashboard">
+        <div className="flex flex-row space-x-4">
+          <BarChart
+            dataset={dataset}
+            xAxis={[{ scaleType: "band", dataKey: "month" }]}
+            {...chartSetting}
+            width={500} // Tamaño igualado
+            height={300} // Tamaño igualado
+          />
+
+          <LineChart
+            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            yAxis={[{ data: [5, 15, 25, 35, 40, 50] }]}
+            series={[
+              {
+                data: [2, 5.5, 2, 8.5, 1.5, 5],
+                area: true,
+              },
+            ]}
+            width={500} // Tamaño igualado
+            height={300} // Tamaño igualado
+          />
+        </div>
+      </BaseScreen>
+    </div>
+  );
 }
