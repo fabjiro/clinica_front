1850cf003c83ca0a664b10166c2d82b22f906c6c
fabjiro <fjiron2001@gmail.com>
2025-02-26 19:47:40 -0600

    add lazy loading for dashboard routes in seller interface

--- src/presentation/screens/Seller/SellerRoutes.tsx
+++ src/presentation/screens/Seller/SellerRoutes.tsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { ShellSellerLayout } from "./ShellSellerLayout";
+import { lazy } from "react";
+
+
+const DashboardRoutesLazy = lazy(() => import("../Dashboard/DashboardRoutes"));
 
 export default function SellerRoutes() {
   return (
     <Routes>
       <Route element={<ShellSellerLayout />}>
+              <Route path="dashboard" element={<DashboardRoutesLazy />} />
+              
       </Route>
     </Routes>
   );
